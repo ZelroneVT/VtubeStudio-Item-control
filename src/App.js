1dec8bcfb107dea2d49bceb866370900e2bde9e0
ZelroneVT <zelronefirestorm@gmail.com>
2022-03-13 04:19:00 +0530

    first model

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import WebsocketElem from "./WebsocketElem";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <WebsocketElem />
       </header>
     </div>
   );
